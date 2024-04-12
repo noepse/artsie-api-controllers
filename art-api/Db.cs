486fac166d57b7dcf408dc4e6094f331f6d671e3
simmi <22905246+noepse@users.noreply.github.com>
2024-04-12 16:26:29 +0100

    fix git push issue

---
+++ art-api/Db.cs
@@ -0,0 +1,91 @@
+namespace Artsie.DB;
+
+public record Art
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Artist { get; set; }
+    public string? Description { get; set; }
+    public string? ImgUrl { get; set; }
+}
+
+public class ArtDB
+{
+    private static List<Art> _art = new List<Art>()
+   {
+     new Art{ Id=1, Name="Cleopatra decorating the Tomb of Mark Anthony", Artist="Angelica Kauffman", Description="The scene portrays Cleopatra in a moment of mourning and love, as she decorates the tomb of Mark Antony, her lover and ally, who had committed suicide after being defeated by Octavian (later Emperor Augustus) in the Battle of Actium in 31 BC. Cleopatra, with a look of sorrow and determination, is shown placing a wreath on Antony's tomb, surrounded by mourners and attendants." },
+     new Art{ Id=2, Name="Wanderer above the Sea of Fog", Artist = "Caspar David Friedrich", Description="The painting depicts a solitary figure standing atop a rocky precipice, gazing out over a vast, misty landscape of mountains and fog-covered valleys. The figure, often interpreted as a representation of the Romantic wanderer or the sublime individual, stands with his back to the viewer, his face obscured, and his identity ambiguous. He is dressed in a dark overcoat and wears a wide-brimmed hat, adding to the sense of mystery and anonymity. The painting is often interpreted as a meditation on the human experience of the sublime—the overwhelming sense of awe, terror, and transcendence inspired by nature's grandeur. "},
+     new Art{ Id=3, Name="The Great Day of His Wrath", Artist = "John Martin", Description="In this painting, Martin depicts a cataclysmic scene of divine judgment and apocalyptic destruction. The painting shows a vast landscape engulfed in chaos and devastation, with fire, brimstone, and volcanic eruptions wreaking havoc upon the earth. In the foreground, terrified figures, including women, men, and children, flee in panic from the impending doom, while others kneel in prayer or despair. The sky is darkened by swirling clouds, lightning bolts, and ominous storm clouds, heightening the sense of dread and impending doom."}
+   };
+
+    public static Art? GetArtById(int id)
+    {
+        return _art.SingleOrDefault(art => art.Id == id);
+    }
+}
+
+public record Comment
+{
+    public int Id { get; set; }
+    public int? ArtId { get; set; }
+    public string? Author { get; set; }
+    public string? Body { get; set; }
+
+    public int? Likes { get; set; }
+
+}
+
+public class CommentsDB
+{
+
+    private static List<Comment> _comments = new List<Comment>()
+   {
+     new Comment{ Id=1, ArtId=1, Author="froggie", Body="Nice art!" },
+   };
+
+
+    public static List<Comment> GetComments()
+    {
+        return _comments;
+    }
+
+    public static Comment CreateComment(Comment comment)
+    {
+        _comments.Add(comment);
+        return comment;
+    }
+
+    public static Comment UpdateComment(Comment update)
+    {
+        _comments = _comments.Select(comment =>
+        {
+            if (comment.Id == update.Id)
+            {
+                comment.Author = update.Author;
+            }
+            return comment;
+        }).ToList();
+        return update;
+    }
+
+    public static void RemoveComment(int id)
+    {
+        _comments = _comments.FindAll(comment => comment.Id != id).ToList();
+    }
+
+}
+public record User
+{
+    public int Id { get; set; }
+    public string? Username { get; set; }
+
+}
+
+public class UserDB
+{
+
+    private static List<User> _users = new List<User>()
+   {
+     new User{ Id=1, Username="froggie"},
+   };
+}
---
+++ art-api/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.OpenApi.Models;
+using Artsie.DB;
+
+var builder = WebApplication.CreateBuilder(args);
+    
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Art API", Description = "Browse some beautiful art", Version = "v1" });
+});
+    
+var app = builder.Build();
+    
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+   c.SwaggerEndpoint("/swagger/v1/swagger.json", "Art API V1");
+});
+    
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/art/{id}", (int id) => ArtDB.GetArtById(id));
+app.MapGet("/comments", () => CommentsDB.GetComments());
+app.MapPost("/comments", (Comment comment) => CommentsDB.CreateComment(comment));
+app.MapPut("/comments", (Comment comment) => CommentsDB.UpdateComment(comment));
+app.MapDelete("/comments/{id}", (int id) => CommentsDB.RemoveComment(id));
+    
+app.Run();
+
+public partial class Program { }
