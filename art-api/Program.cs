1b3494e715ef575ebe74efcb088e9eec5df7e828
simmi <22905246+noepse@users.noreply.github.com>
2024-04-16 18:23:58 +0100

    add doc values for root and getArtById

--- art-api/Program.cs
+++ art-api/Program.cs
@@ -17,7 +17,14 @@ app.UseSwaggerUI(c =>
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Art API V1");
 });
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!")
+.WithOpenApi(operation => 
+    {
+        operation.Summary = "Returns the root endpoint";
+        operation.Description = "Returns Hello World!";
+
+        return operation;
+    });
 
 app.MapGet("/art/{id}", (int id) =>
 {
@@ -32,7 +39,17 @@ app.MapGet("/art/{id}", (int id) =>
         // Return the art object
         return Results.Ok(art);
     }
-});
+}).WithOpenApi(operation => 
+    {
+        operation.Summary = "Returns art data for given ID";
+        operation.Description = "Returns an object containing the name, artist, description and image url of the requested art.";
+        operation.Parameters[0].Description = "An integer that references the Art ID of the requested art";
+        return operation;
+        
+    }).Produces<Art>(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status400BadRequest)
+    .Produces(StatusCodes.Status404NotFound);
+
 app.MapGet("/art/{id}/comments", (int id) =>
 {
     var comments = CommentsDB.GetCommentsByArtId(id);
