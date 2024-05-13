d5a42bef9257aa6ca5e5a4b65153e96cbf06aa61
simmi <22905246+noepse@users.noreply.github.com>
2024-05-13 17:14:54 +0100

    add dockerFile

--- art-api/obj/Debug/net8.0/art-api.AssemblyInfo.cs
+++ art-api/obj/Debug/net8.0/art-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("art-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e1d94dab19cc6eb087357717cf902ab430edad2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d90a1facafeeda700404fe4726431137f80773b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("art-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("art-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
